b4779accfd44fd49be6b8eb10491016298e01b15
cesar arroyo <cesararroyo1234@gmail.com>
2015-04-17 12:10:31 -0700

    milestone 6

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli =new mysqli('localhost', 'root', 'root', 'taasks');
+$mysqli =new mysqli('localhost', 'root', 'root', 'todo');
 if($mysqli->connect_error){
 	die('Connect Error ('. $mysqli->connect_erno .')'
 		.$mysqli->connect_error);
