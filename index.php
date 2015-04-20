9a36d3401c5765f099cc45cca07d3fe60d074ecf
cesar arroyo <cesararroyo1234@gmail.com>
2015-04-19 21:54:25 -0700

    video 15

--- index.php
+++ index.php
@@ -6,8 +6,15 @@
 <link rel="stylesheet" type="text/css" href="css/main.css">
 
 <body>
-<div class="wrap">
-
-</div>
+ <div class="wrap">
+ 		<div class="task-list">
+ 				<ul>
+ 						<?php require("includes/connect.php"); ?>
+ 				</ul>
+ 		</div>
+ </div>
+ <form class="add-new-task"autocomplete="off">
+ 		<input type="text" name="new-text" placeholder="Add new item..."/>
+ 		</form>
 </body>
 </html>
