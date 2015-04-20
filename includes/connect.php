1791bcdf32eab6351634f0a03e279a80736f03d3
cesar arroyo <cesararroyo1234@gmail.com>
2015-04-19 22:21:22 -0700

    all files repushing

---
+++ includes/connect.php
@@ -0,0 +1,16 @@
+<?php
+	//Why is this important? Why dose localhost have to be first? 
+	$mysqli = new mysqli('localhost', 'root', 'root', 'todo');
+	//mysqli->connect_error then we want it to die and have this message 
+	if ($mysqli->connect_error) 
+	{
+	  die('Connect Error (' . $mysqli->connect_error .')' .$mysqli->connect_error);	  	
+	}
+	else 
+	{
+		echo "Connection made";
+	}
+	$mysqli->close();
+
+	
+	?>
--- index.php
+++ index.php
@@ -1,20 +1,60 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>simple todo-list</title>
+	<title> Donaji's To-Do List </title>
+
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+
 </head>
-<link rel="stylesheet" type="text/css" href="css/main.css">
 
 <body>
- <div class="wrap">
- 		<div class="task-list">
- 				<ul>
- 						<?php require("includes/connect.php"); ?>
- 				</ul>
- 		</div>
- </div>
- <form class="add-new-task"autocomplete="off">
- 		<input type="text" name="new-text" placeholder="Add new item..."/>
- 		</form>
+
+
+	<div class="wrap">
+	<div clss="task-list">
+		<ul>
+			<?php require("includes/connect.php"); ?>
+
+
+
+			?>
+		</ul>	
+	</div>
+	<form class="add-new-text" autocomplete="off">
+		<input type="text" name="new-task" placeholder="Add new item..."/>
+	</form>
+	</div>	
 </body>
+<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script>
+	add_task(); //calling the add task function
+
+	function add_task(){
+		$('.add-new-task').submit(function() {
+			var new_task = $('.add-new-task input[name=new-task]').val();
+
+			if (new_task != '') {
+				$.post('includes/add-task.php', {task: new_task}, function(data) {
+					$('add-new-task input[name=new-task]').val();
+					  $(data).appendTo('task-list ul').hide().fadeIn();
+				});
+			}
+			return false;
+		});
+	}
+
+	$('.delete-button').click(function(){
+		var current_element = $(this);
+		var task_id = $(this).attr('id');
+
+		$.post('includes/delete-task.php', {id: task_id}, function(){
+		current_element.parent(.fadeOut("fast", function(){
+			$(this).remove();
+		});
+	});
+});		
+</script>
+
+
 </html>
